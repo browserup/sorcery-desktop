6294f7216fce10ae8f31ca07bfb9623b683400ba
Eric Beland <ebeland@gmail.com>
2025-11-18 20:21:48 -0500

    Module exports

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,11 @@
+// Library interface for testing
+// This exposes internal modules for integration testing
+
+pub mod dispatcher;
+pub mod editors;
+pub mod git_command_log;
+pub mod path_validator;
+pub mod protocol_handler;
+pub mod settings;
+pub mod tracker;
+pub mod workspace_mru;
